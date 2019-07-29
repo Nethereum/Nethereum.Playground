55d3c86d00a93727228a5401261ddccd89bb054d
Juan Blanco <juanfranblanco@hotmail.com>
2019-07-29 21:09:59 +0100

    Update CSCodeSampleRepository.cs

--- Nethereum.TryOnBrowser/Pages/CSCodeSampleRepository.cs
+++ Nethereum.TryOnBrowser/Pages/CSCodeSampleRepository.cs
@@ -1,9 +1,56 @@
-﻿namespace Nethereum.TryOnBrowser.Pages
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Nethereum.TryOnBrowser.Pages
 {
+
+    public class GithubContent
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+
+    }
     public class CodeSampleRepository
     {
-        public CodeSample[] GetCodeSamples()
+        private HttpClient _httpClient;
+        private const string _githubPath = "nethereum/nethereum/nethereum.playground";
+
+        private const string _githubContentUrl =
+            "https://api.github.com/repos/Nethereum/Nethereum.Playground/contents/samples/csharp";
+
+        private const string _githubFileUrl =
+            "https://raw.githubusercontent.com/Nethereum/Nethereum.Playground/master/samples/csharp/";
+
+        private List<CodeSample> _codeSamples;
+
+        public CodeSampleRepository(HttpClient httpClient)
         {
+            _httpClient = httpClient;
+            _codeSamples = new List<CodeSample>();
+        }
+
+        public async Task<CodeSample[]> GetCodeSamples()
+        {
+            //var contents = await _httpClient.GetJsonAsync<GithubContent[]>(_githubContentUrl);
+            //Console.WriteLine(contents.Length);
+            //Console.WriteLine(contents[0].Name);
+
+            
+            //foreach (var content in contents)
+            //{
+            //    //var code = await _httpClient.GetStringAsync(_githubFileUrl + content.Name);
+                    
+            //    _codeSamples.Add(new CodeSample()
+            //    {
+            //        Name = content.Name,
+            //       // Code = code   
+            //    });
+            //}
+
+            //return _codeSamples.ToArray();
             return new CodeSample[]
             {
 
@@ -677,7 +724,55 @@ public class BlockProcessing_StartHere
         }
     }
 }"
-                }					
+                },
+
+ new CodeSample()
+                {
+                    Name = "Block Crawl Processing: Transaction criteria",
+                    Code = @"
+using Nethereum.BlockchainProcessing.Processor;
+using Nethereum.RPC.Eth.DTOs;
+using Nethereum.Web3;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class BlockProcessing_WithTransactionCriteria
+{
+    /// <summary>
+    /// Process transactions matching specific criteria
+    /// </summary>
+    /// <returns></returns>
+    public static async Task Main()
+    {
+        var transactions = new List<TransactionReceiptVO>();
+        var filterLogs = new List<FilterLogVO>();
+
+        var web3 = new Web3(""https://rinkeby.infura.io/v3/7238211010344719ad14a89db874158c"");
+
+        //create our processor
+        var processor = web3.Processing.Blocks.CreateBlockProcessor(steps => {
+            steps.TransactionStep.SetMatchCriteria(t => t.Transaction.IsFrom(""0x1cbff6551b8713296b0604705b1a3b76d238ae14""));
+            steps.TransactionReceiptStep.AddSynchronousProcessorHandler(tx => transactions.Add(tx));
+            steps.FilterLogStep.AddSynchronousProcessorHandler(l => filterLogs.Add(l));
+        });
+
+        //if we need to stop the processor mid execution - call cancel on the token
+        var cancellationToken = new CancellationToken();
+        //crawl the blocks
+        await processor.ExecuteAsync(
+            toBlockNumber: new BigInteger(2830145),
+            cancellationToken: cancellationToken,
+            startAtBlockNumberIfNotProcessed: new BigInteger(2830144));
+
+        Console.WriteLine($""Transactions. Expected: 2, Actual: {transactions.Count}"");
+        Console.WriteLine($""Logs. Expected: 4, Actual: {filterLogs.Count}"");
+    }
+
+}"
+                }				
 
             };
         }
