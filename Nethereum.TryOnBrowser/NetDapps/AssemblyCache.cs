2567b81ca1c491535ec9fdb49c4815017fec85e6
Juan Blanco <juanfranblanco@hotmail.com>
2019-08-06 19:41:54 +0100

    Adding more libraries to ipfs

--- Nethereum.TryOnBrowser/NetDapps/AssemblyCache.cs
+++ Nethereum.TryOnBrowser/NetDapps/AssemblyCache.cs
@@ -9,30 +9,6 @@ using Microsoft.CodeAnalysis;
 
 namespace NetDapps.Assemblies
 {
-	
-    public class AssemblyLoadInfo
-    {
-        public AssemblyLoadInfo()
-        {
-            
-        }
-
-        public AssemblyLoadInfo(string fullName)
-        {
-            FullName = fullName;
-        }
-
-        public AssemblyLoadInfo(string fullName, string publishedRemotePath) : this(fullName)
-        {
-            PublishedRemotePath = publishedRemotePath;
-        }
-
-        public string FullName { get; set; }
-        public string PublishedRemotePath { get; set; }
-		public MetadataReference MetadataReference { get; set;}
-
-    }
-
     public class AssemblyCache
     {
         private static AssemblyCache _current;
---
+++ Nethereum.TryOnBrowser/NetDapps/AssemblyLoadInfo.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace NetDapps.Assemblies
+{
+    public class AssemblyLoadInfo
+    {
+        public AssemblyLoadInfo()
+        {
+            
+        }
+
+        public AssemblyLoadInfo(string fullName)
+        {
+            FullName = fullName;
+        }
+
+        public AssemblyLoadInfo(string fullName, string publishedRemotePath) : this(fullName)
+        {
+            PublishedRemotePath = publishedRemotePath;
+        }
+
+        public string FullName { get; set; }
+        public string PublishedRemotePath { get; set; }
+        public MetadataReference MetadataReference { get; set;}
+
+    }
+}
