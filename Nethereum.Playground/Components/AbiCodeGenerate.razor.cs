40f33d4f77e4fa53a2233933e39281e005d1c894
Dave-Whiffin <dave.whiffin@gmail.com>
2019-09-26 16:37:30 +0100

    abi and byte code file picker for code gen

--- Nethereum.Playground/Components/AbiCodeGenerate.razor.cs
+++ Nethereum.Playground/Components/AbiCodeGenerate.razor.cs
@@ -10,8 +10,28 @@ namespace Nethereum.Playground.Components
 {
     public class AbiCodeGenerateBase: ComponentBase
     {
+        [Inject] public IFileReaderService FileReaderService { get; set; }
+
         [Parameter]
         public AbiCodeGenerateModel Model { get; set; }
 
+        public ElementReference ABIFileUpload { get; set; }
+        public ElementReference ByteCodeFileUpload { get; set; }
+
+        public async Task ReadFileABI()
+        {
+            var files = await FileReaderService.CreateReference(ABIFileUpload).EnumerateFilesAsync();
+            var stream = await files.First().CreateMemoryStreamAsync();
+            var streamReader = new System.IO.StreamReader(stream);
+            Model.Abi = streamReader.ReadToEnd();
+        }
+
+        public async Task ReadFileByteCode()
+        {
+            var files = await FileReaderService.CreateReference(ByteCodeFileUpload).EnumerateFilesAsync();
+            var stream = await files.First().CreateMemoryStreamAsync();
+            var streamReader = new System.IO.StreamReader(stream);
+            Model.ByteCode = streamReader.ReadToEnd();
+        }
     }
 }
